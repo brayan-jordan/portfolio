6e155cfea08f2f1aac06d4f24283a44ee2cb58db
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2022-10-14 16:42:00 -0300

    Melhorias no padrao de estilizacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  aboutMe = "Olá, meu nome é Brayan, seja bem vindo ao meu portifólio!"
+  aboutMe2 = "Tive meu primeiro contato com programaçaõ em 2020, desde lá me apaixonei pela área e busco evoluir cada dia mais."
+  aboutMe3 = "As tecnologias que eu trabalho/estudo são: "
+  myTechs = "NodeJS, Angular, ReactJS, .NET, Java, HTML, CSS, JavaScript, TypeScript, C#, MySQL, SQL Server"
 }
